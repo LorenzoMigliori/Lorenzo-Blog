e5057eea2fb35b53e4ce4e2d483ced6cdd63eee9
Lorenzo <migliori.lorenzo@gmail.com>
2022-01-31 17:34:47 +0100

    Can now edit posts

--- main.py
+++ main.py
@@ -81,12 +81,12 @@ gravatar = Gravatar(app,
 #Create admin-only decorator
 def admin_only(f):
     @wraps(f)
-    def decorated_function():
+    def decorated_function(*args, **kwargs):
         #If id is not 1 then return abort with 403 error
         if not current_user.is_authenticated or current_user.id != 1:
             return abort(403)
         #Otherwise continue with the route function
-        return f()
+        return f(*args, **kwargs)
     return decorated_function
 
 @login_manager.user_loader
